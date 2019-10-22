3a3a9902effd62480dde32da78b930374849a207
Fabrizio Pastore <fabrizio.pastore@gmail.com>
2019-10-22 21:59:48 +0200

    LICENCE fix

--- src-bct-porting/gui/JMultiLineToolTip.java
+++ src-bct-porting/gui/JMultiLineToolTip.java
@@ -1,28 +1,18 @@
-/* -- JFLAP 4.0 --
+/*******************************************************************************
+ *    Copyright 2019 Fabrizio Pastore, Leonardo Mariani
+ *   
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * Copyright information:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Susan H. Rodger, Thomas Finley
- * Computer Science Department
- * Duke University
- * April 24, 2003
- * Supported by National Science Foundation DUE-9752583.
- *
- * Copyright (c) 2003
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the author.  The name of the author may not be used to
- * endorse or promote products derived from this software without
- * specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *******************************************************************************/
  
 // I lifted this code directly from http://www.codeguru.com/java/articles/122.shtml.
 
--- src-bct-porting/sjm/parse/tokens/Word.java
+++ src-bct-porting/sjm/parse/tokens/Word.java
@@ -4,13 +4,21 @@ import java.util.Vector;
 
 import sjm.parse.Parser;
 import sjm.parse.Terminal;
-/*
- * Copyright (c) 1999 Steven J. Metsker. All Rights Reserved.
- * 
- * Steve Metsker makes no representations or warranties about
- * the fitness of this software for any particular purpose, 
- * including the implied warranty of merchantability.
- */
+/*******************************************************************************
+ *    Copyright 2019 Fabrizio Pastore, Leonardo Mariani
+ *   
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *******************************************************************************/
 
 /**
  * A Word matches a word from a token assembly.
